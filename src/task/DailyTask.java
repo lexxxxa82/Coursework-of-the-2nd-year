8f5b47bb9fb0c8870624d1884013225e48bbcd67
lexxxxa82 <lexxxxa.82@mail.ru>
2023-02-20 21:25:50 +0800

    1

---
+++ src/task/DailyTask.java
@@ -0,0 +1,11 @@
+package task;
+
+import java.time.LocalDate;
+
+public class DailyTask extends Task {
+
+    @Override
+    public boolean appearsIn(LocalDate localDate) {
+        return true;
+    }
+}
---
+++ src/task/MonthlyTask.java
@@ -0,0 +1,11 @@
+package task;
+
+import java.time.LocalDate;
+
+public class MonthlyTask extends Task {
+
+    @Override
+    public boolean appearsIn(LocalDate localDate) {
+        return localDate.getDayOfMonth() == getDateTime().toLocalDate().getDayOfMonth();
+    }
+}
