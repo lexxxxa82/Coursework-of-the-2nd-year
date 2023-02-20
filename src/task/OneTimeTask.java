a079ee60eb66d428069f9fea43fa0fc99f775e88
lexxxxa82 <lexxxxa.82@mail.ru>
2023-02-20 21:25:50 +0800

    1

---
+++ src/task/OneTimeTask.java
@@ -0,0 +1,11 @@
+package task;
+
+import java.time.LocalDate;
+
+public class OneTimeTask extends Task {
+
+    @Override
+    public boolean appearsIn(LocalDate localDate) {
+        return localDate.equals(getDateTime().toLocalDate());
+    }
+}
